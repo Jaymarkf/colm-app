475768cab4265e2c0a0b00ce74cc44afb80beaac
MAXX <fernandesjaymark@yahoo.com>
2022-07-20 23:13:33 +0800

    header

---
+++ resources/views/sections/header.blade.php
@@ -0,0 +1,18 @@
+<header class="xl:py-2 xl:flex">
+    <div class="xl:p-1 w-28">
+        <img class="xl:w-full xl:m-0" loading="lazy" src="{{asset('storage/images/logo.png')}}">
+    </div>
+    <div class="flex xl:justify-between xl:items-center xl:pl-2 xl:w-full ">
+            <h1 class="xl:text-2xl xl:font-bold">College of Our Lady Of Mercy Of Pulilan Foundation. INC.</h1>
+            <div class="xl:flex xl:items-center">
+                <a href="#" class="xl:text-xl font-bold xl:w-44">About</a>
+                <a href="#" class="xl:text-xl font-bold xl:w-44">Programs</a>
+                <a href="#" class="xl:text-xl font-bold xl:w-44">Admision</a>
+                <a href="#" class="xl:text-xl font-bold xl:w-44">FAQs</a>
+                <div class="xl:flex xl:flex-col">
+                    <a href="#" class="xl:text-xl font-bold xl:w-44 mb-2">Enquire Now</a>
+                    <a href="#" class="xl:text-xl font-bold xl:w-44 mt-2">Enroll Now</a>
+                </div>
+            </div>
+    </div>
+</header>
