b794e80a357d9e7897f432975fd59246a3327fe5
MAXX <fernandesjaymark@yahoo.com>
2022-07-21 23:41:26 +0800

    blurb redefine

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -4,7 +4,7 @@
         <img class="xl:w-4/5 2xl:w-full  2xl:m-0" loading="lazy" src="{{asset('storage/images/logo.png')}}">
     </div>
     <div class="flex flex-col justify-center sm:items-center sm:pl-2 w-full   md:justify-between md:flex-row">
-            <h1 class="font-bold mb-4 text-xl xl:text-base sm:font-bold 2xl:text-2xl">College of Our Lady Of Mercy Of<br class="block md:hidden"> Pulilan Foundation. INC.</h1>
+            <h1 class="font-bold mb-4 text-xl xl:text-base sm:font-bold 2xl:text-2xl">COLLEGE OF OUR LADY OF MERCY OF <br class="block md:hidden"> PULILAN FOUNDATION. INC.</h1>
             <ul class="flex items-center hidden md:flex">
                 <li class="li-header w-24 2xl:w-40">
                     <a href="#" class="hover:text-emerald-600 font-medium 2xl:text-xl" >About</a>
