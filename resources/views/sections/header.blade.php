c2e50d6754bb9a613c8658179cdcbd1b2e3be801
MAXX <fernandesjaymark@yahoo.com>
2022-07-26 23:50:57 +0800

    header updated

--- resources/views/sections/header.blade.php
+++ resources/views/sections/header.blade.php
@@ -1,67 +1,106 @@
-<header class="flex justify-center flex-col items-center md:flex md:flex-row md:px-4 2xl:py-4 xl:px-5 2xl:px-10 shadow-md mheader">
+<header class="flex justify-center flex-col items-center md:flex md:flex-row md:px-4 xl:py-3 2xl:py-4 xl:px-5 2xl:px-10 shadow-md mheader">
     <div class="py-5 xl:p-2 w-28 ">
         <img class="xl:w-4/5 2xl:w-full 2xl:m-0 w-36" loading="lazy" src="{{asset('storage/images/logo.webp')}}" alt="main_logo">
     </div>
     <div class="flex flex-col justify-center items-center text-center sm:pl-2 w-full  md:justify-between md:flex-row">
-            <h1 class="font-bold mb-4 md:mb-0 text-xl text-center xl:text-base sm:font-bold 2xl:text-2xl">COLLEGE OF OUR LADY OF MERCY OF <br class="block md:hidden"> PULILAN FOUNDATION. INC.</h1>
-            <ul class="flex items-center hidden md:flex">
-                <li class="li-header w-24 2xl:w-40 relative">
-                    <div href="#" class="hover:text-emerald-600 font-medium 2xl:text-xl hdropdown-desktop cursor-pointer" >About <i class="fa fa-caret-down"></i>
-                    <ul class="hidden absolute hdcontent bg-white shadow-md hdcontent-desktop subNav abo_subnav">
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">School Profile</a>
+            <h1 class="font-bold mb-4 md:mb-0 text-xl text-center xl:text-base sm:font-bold 2xl:text-2xl">COLLEGE OF OUR LADY OF MERCY OF <br class="block md:hidden"> PULILAN FOUNDATION, INC.</h1>
+            <ul class="flex items-center">
+                <div class="flex flex-col items-center">
+                    <ul class="hidden md:flex items-center justify-evenly w-full mb-5">
+                        <li>
+                        <img src="{{asset('storage/images/deped.webp')}}" data-src="{{asset('storage/images/deped.webp')}}" 
+                            data-srcset="
+                            {{asset('storage/images/deped.webp')}} 300w,
+                            {{asset('storage/images/deped.webp')}} 600w,
+                            {{asset('storage/images/deped.webp')}} 900w,
+                            " class="lazyload w-28" data-sizes="auto" alt="">
                         </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">Virtual Tour</a>
+                        <li>
+                        <img src="{{asset('storage/images/up.webp')}}" data-src="{{asset('storage/images/up.webp')}}" 
+                            data-srcset="
+                            {{asset('storage/images/up.webp')}} 300w,
+                            {{asset('storage/images/up.webp')}} 600w,
+                            {{asset('storage/images/up.webp')}} 900w,
+                            " class="lazyload w-14" data-sizes="auto" alt="">
                         </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">Contact Us</a>
+                        <li>
+                        <img src="{{asset('storage/images/ched.webp')}}" data-src="{{asset('storage/images/ched.webp')}}" 
+                            data-srcset="
+                            {{asset('storage/images/ched.webp')}} 300w,
+                            {{asset('storage/images/ched.webp')}} 600w,
+                            {{asset('storage/images/ched.webp')}} 900w,
+                            " class="lazyload w-14" data-sizes="auto" alt="">  
                         </li>
-                    </ul>
-                    </div>
-                </li>
-                <li class="li-header w-24  2xl:w-40 relative md:mr-3">
-                    <div class="hover:text-emerald-600 font-medium 2xl:text-xl hdropdown-desktop cursor-pointer" >Programs <i class="fa fa-caret-down"></i>
-                    <ul class="hidden absolute hdcontent bg-white shadow-md hdcontent-desktop subNav prog_subnav">
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">Pre Elem</a>
-                        </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">Elem</a>
-                        </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">Junior HS</a>
-                        </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">Senior HS</a>
-                        </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">College</a>
-                        </li>
-                        <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                            <a href="#" class="label-color hover:text-emerald-600">TESDA</a>
+                        <li>
+                            <img src="{{asset('storage/images/tesda.webp')}}" data-src="{{asset('storage/images/tesda.webp')}}" 
+                                data-srcset="
+                                {{asset('storage/images/tesda.webp')}} 300w,
+                                {{asset('storage/images/tesda.webp')}} 600w,
+                                {{asset('storage/images/tesda.webp')}} 900w,
+                                " class="lazyload w-14" data-sizes="auto" alt="">
                         </li>
                     </ul>
-                    </div>
-                </li>
-                <li class="li-header w-24 2xl:w-40 relative md:mr-3">
-                    <div class="hover:text-emerald-600 font-medium 2xl:text-xl hdropdown-desktop cursor-pointer" >Admission <i class="fa fa-caret-down"></i>
-                        <ul class="hidden absolute hdcontent bg-white shadow-md hdcontent-desktop subNav adm_subnav">
-                            <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                                <a href="#" class="label-color hover:text-emerald-600">Process</a>
-                            </li>
-                            <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
-                                <a href="#" class="label-color hover:text-emerald-600">Requirements</a>
-                            </li>
-                        </ul>
-                    </div>
-                </li>
-                <li class="li-header w-24  2xl:w-40 relative text-center">
-                    <a href="#" class="hover:text-emerald-600 font-medium 2xl:text-xl" >FAQs</a>
-                </li>
-                <li class="li-header  w-24 2xl:w-40 flex flex-col justify-center items-center">
-                   <a href="#" class="mb-2 hover:text-emerald-600 font-bold 2xl:text-xl">Inquire Now</a>
-                   <a href="#" class="mt-2  hover:text-emerald-600 font-bold 2xl:text-xl">Enroll Now</a>
+                    <ul class="flex items-center hidden md:flex">
+                        <li class="li-header w-24 2xl:w-40 relative">
+                            <div href="#" class="hover:text-emerald-600 font-medium 2xl:text-xl hdropdown-desktop cursor-pointer" >About <i class="fa fa-caret-down"></i>
+                            <ul class="hidden absolute hdcontent bg-white shadow-md hdcontent-desktop subNav abo_subnav">
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">School Profile</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">Virtual Tour</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">Contact Us</a>
+                                </li>
+                            </ul>
+                            </div>
+                        </li>
+                        <li class="li-header w-24  2xl:w-40 relative md:mr-3">
+                            <div class="hover:text-emerald-600 font-medium 2xl:text-xl hdropdown-desktop cursor-pointer" >Programs <i class="fa fa-caret-down"></i>
+                            <ul class="hidden absolute hdcontent bg-white shadow-md hdcontent-desktop subNav prog_subnav">
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">Pre Elem</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">Elem</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">Junior HS</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">Senior HS</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">College</a>
+                                </li>
+                                <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                    <a href="#" class="label-color hover:text-emerald-600">TESDA</a>
+                                </li>
+                            </ul>
+                            </div>
+                        </li>
+                        <li class="li-header w-24 2xl:w-40 relative md:mr-3">
+                            <div class="hover:text-emerald-600 font-medium 2xl:text-xl hdropdown-desktop cursor-pointer" >Admission <i class="fa fa-caret-down"></i>
+                                <ul class="hidden absolute hdcontent bg-white shadow-md hdcontent-desktop subNav adm_subnav">
+                                    <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                        <a href="#" class="label-color hover:text-emerald-600 leading-none">Enrollment Process</a>
+                                    </li>
+                                    <li class="text-center border-b border-stone-200 h-10  justify-center flex items-center">
+                                        <a href="#" class="label-color hover:text-emerald-600">Requirements</a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </li>
+                        <li class="li-header w-24  2xl:w-40 relative text-center">
+                            <a href="#" class="hover:text-emerald-600 font-medium 2xl:text-xl" >FAQs</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <li class="li-header  w-24 2xl:w-40 flex flex-col justify-center items-center w-56 hidden md:flex">
+                   <a href="#" class="mb-2 hover:text-emerald-600 font-bold 2xl:text-xl bg-emerald-800 text-white py-1 px-2 w-40 p-2 rounded-md hover:bg-lime-500 hover:text-white" style="color:white;">Inquire Now</a>
+                   <a href="#" class="mt-2  hover:text-emerald-600 font-bold 2xl:text-xl bg-emerald-800 text-white py-1 px-2 w-40 p-2 rounded-md hover:bg-lime-500 hover:text-white" style="color:white;">Enroll Now</a>
                 </li>
             </ul>
             <div class="p-4 space-y-2 bg-emerald-800 rounded shadow block  self-end mr-4 mb-4 md:hidden hm">
@@ -69,7 +108,7 @@
                 <span class="block h-0.5 bg-gray-100 animate-pulse"></span>
                 <span class="block h-0.5 bg-gray-100 animate-pulse"></span>
             </div>
-            <ul class="hidden hm-content md:hidden">
+            <ul class="hidden hm-content md:hidden w-full">
                 <li class="my-4 relative">
                     <span class="hover:text-emerald-600 text-lg font-bold hdropdown">About <i class="fa fa-caret-down"></i></span>
                     <ul class="hidden relative hdcontent hdcontent-mobile">
@@ -111,7 +150,7 @@
                     <span class="text-lg font-bold hover:text-emerald-600 hdropdown">Admission <i class="fa fa-caret-down"></i></span>
                     <ul class="hidden relative hdcontent hdcontent-mobile">
                         <li>
-                            <a href="#" class="label-color hover:text-emerald-600">Process</a>
+                            <a href="#" class="label-color hover:text-emerald-600 leading-none">Enrollment Process</a>
                         </li>
                         <li>
                             <a href="#" class="label-color hover:text-emerald-600">Requirements</a>
@@ -127,6 +166,42 @@
                 <li class="my-4">
                     <a href="#" class="text-lg font-bold hover:text-emerald-600">Enroll Now</a>
                 </li>
+                <ul>
+                <ul class="flex md:hidden items-center justify-evenly w-full mb-5">
+                        <li>
+                        <img src="{{asset('storage/images/deped.webp')}}" data-src="{{asset('storage/images/deped.webp')}}" 
+                            data-srcset="
+                            {{asset('storage/images/deped.webp')}} 300w,
+                            {{asset('storage/images/deped.webp')}} 600w,
+                            {{asset('storage/images/deped.webp')}} 900w,
+                            " class="lazyload w-28" data-sizes="auto" alt="">
+                        </li>
+                        <li>
+                        <img src="{{asset('storage/images/up.webp')}}" data-src="{{asset('storage/images/up.webp')}}" 
+                            data-srcset="
+                            {{asset('storage/images/up.webp')}} 300w,
+                            {{asset('storage/images/up.webp')}} 600w,
+                            {{asset('storage/images/up.webp')}} 900w,
+                            " class="lazyload w-14" data-sizes="auto" alt="">
+                        </li>
+                        <li>
+                        <img src="{{asset('storage/images/ched.webp')}}" data-src="{{asset('storage/images/ched.webp')}}" 
+                            data-srcset="
+                            {{asset('storage/images/ched.webp')}} 300w,
+                            {{asset('storage/images/ched.webp')}} 600w,
+                            {{asset('storage/images/ched.webp')}} 900w,
+                            " class="lazyload w-14" data-sizes="auto" alt="">  
+                        </li>
+                        <li>
+                            <img src="{{asset('storage/images/tesda.webp')}}" data-src="{{asset('storage/images/tesda.webp')}}" 
+                                data-srcset="
+                                {{asset('storage/images/tesda.webp')}} 300w,
+                                {{asset('storage/images/tesda.webp')}} 600w,
+                                {{asset('storage/images/tesda.webp')}} 900w,
+                                " class="lazyload w-14" data-sizes="auto" alt="">
+                        </li>
+                    </ul>
+                </ul>
             </ul >
     </div>
 </header>
