e4fa3a30e61da5d9143e790c787d77a659e8e120
Xamm <39992510+Jaymarkf@users.noreply.github.com>
2023-08-30 19:10:58 +0800

    adding inner pages in programs

--- resources/views/admin-dashboard/pages/manage-carousel.blade.php
+++ resources/views/admin-dashboard/pages/manage-carousel.blade.php
@@ -67,8 +67,11 @@
     @endif
     </main>
     <footer>    
-        <!-- Main modal -->
-        <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full">
+
+    </footer>
+
+            <!-- Main modal -->
+            <div id="defaultModal" tabindex="-1" aria-hidden="true" class="hidden overflow-y-auto overflow-x-hidden absolute top-0 right-0 left-0 z-50 w-full md:inset-0 h-modal md:h-full p-3">
             <div class="relative p-4 w-full max-w-2xl h-full md:h-auto">
                 <!-- Modal content -->
                 <div class="relative bg-white rounded-lg shadow dark:bg-gray-700">
@@ -119,7 +122,6 @@
                 </div>
             </div>
         </div>
-    </footer>
         
 </body>
 <script type="text/javascript" src="{{asset('js/custom-script.js')}}"></script>
